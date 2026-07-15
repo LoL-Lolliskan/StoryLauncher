34324f5f10672c7f8d2dd1aee2ebf296533ecf18
LoL-Lolliskan <hackpro24739@gmail.com>
2026-07-15 06:04:16 +0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using StoryLauncher.Views;
+
+namespace StoryLauncher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowHomePage();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHomePage();
+        }
+
+        private void ModpackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ModpackPage());
+            PageTitle.Text = "Модпак";
+
+            HomeButton.Style =
+                (Style)FindResource("NavigationButtonStyle");
+
+            ModpackButton.Style =
+                (Style)FindResource("SelectedNavigationButtonStyle");
+
+            SettingsButton.Style =
+                (Style)FindResource("NavigationButtonStyle");
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new SettingsPage());
+            PageTitle.Text = "Настройки";
+
+            HomeButton.Style =
+                (Style)FindResource("NavigationButtonStyle");
+
+            ModpackButton.Style =
+                (Style)FindResource("NavigationButtonStyle");
+
+            SettingsButton.Style =
+                (Style)FindResource("SelectedNavigationButtonStyle");
+        }
+
+        private void ShowHomePage()
+        {
+            MainFrame.Navigate(new HomePage());
+            PageTitle.Text = "Главная";
+
+            HomeButton.Style =
+                (Style)FindResource("SelectedNavigationButtonStyle");
+
+            ModpackButton.Style =
+                (Style)FindResource("NavigationButtonStyle");
+
+            SettingsButton.Style =
+                (Style)FindResource("NavigationButtonStyle");
+        }
+    }
+}
---
+++ Views/ModpackPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StoryLauncher.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ModpackPage.xaml
+    /// </summary>
+    public partial class ModpackPage : Page
+    {
+        public ModpackPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
